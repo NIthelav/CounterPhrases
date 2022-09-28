7c0632e42ff2e052ac8ac52e26e495bf433b0892
Nithelav <nithelav@yandex.ru>
2022-09-28 17:17:23 +0300

    Add persist/redux

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,24 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { counterReducer } from "./counterSlice/slice";
+import storage from "redux-persist/lib/storage";
+import { persistCombineReducers, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistCombineReducers(persistConfig, {
+  counter: counterReducer,
+});
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: persistedReducer,
+  middleware: [thunk],
 });
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
